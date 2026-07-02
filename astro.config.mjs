1a9fbb108aa93b8bed82a2a77491ca4e3afa17da
CodexErrata <jack.li.jansson@gmail.com>
2026-07-03 07:58:45 +1000

    back to blog

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-
-export default defineConfig({
-  site: "https://www.alaskafilmtv.com",
-  integrations: [sitemap()],
-  output: "static"
-});
